5e3e172c491de22ce4f38100f1c36923bdfe7423
Cabral369 <nicolas.cabral.carvalho@gmail.com>
2025-02-03 23:49:23 -0300

    Add Prisma Neon integration, implement product retrieval by slug, and create Badge component for product status

--- lib/actions/products.actions.ts
+++ lib/actions/products.actions.ts
@@ -1,18 +1,26 @@
 'use server';
-
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/db/prisma";
 import { convertToPlanObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
 
-export async function getLatestProducts() {
-    const prisma = new PrismaClient();
 
+//get latest products
+export async function getLatestProducts() {
     const data = await prisma.product.findMany({
         take:LATEST_PRODUCTS_LIMIT,
         orderBy: {
             createdAt: 'desc'
         }
     })
-
+    
     return convertToPlanObject(data); //precisamos converter o objeto prisma para um objeto plano
+}
+
+//get single products by slug
+export async function getProductBySlug(slug: string){
+    return await prisma.product.findFirst({
+        where:{
+            slug:slug
+        }
+    })
 }
