63b496746b8ff4b0551921a2fa77340075ee9a70
Cabral369 <nicolas.cabral.carvalho@gmail.com>
2025-02-04 21:54:01 -0300

    Add ProductImages component and update product details page layout; change product ID type to string

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import ProductPrice from "@/components/shared/product/product-price";
+import ProductImages from "@/components/shared/product/product-images";
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -12,16 +13,16 @@ const ProductDetailsPage = async (props: {
 
   const product = await getProductBySlug(slug);
 
-  if (!product) {
-    return notFound();
-  }
+  if (!product) return notFound();
 
   return (
     <>
       <section>
         <div className="grid grid-cols-1 md:grid-cols-5">
           {/* Images columns */}
-          <div className="col-span-2"></div>
+          <div className="col-span-2">
+            <ProductImages images={product.images} />
+          </div>
           <div className="col-span-2 p-5">
             <div className="flex flex-col gap-6">
               <p>
@@ -44,27 +45,29 @@ const ProductDetailsPage = async (props: {
             </div>
           </div>
           <div>
-            <CardContent className="p-4">
-              <div className="mb-2 flex justify-between">
-                <div>Price</div>
-                <div>
-                  <ProductPrice value={Number(product.price)} />
+            <Card>
+              <CardContent className="p-4">
+                <div className="mb-2 flex justify-between">
+                  <div>Price</div>
+                  <div>
+                    <ProductPrice value={Number(product.price)} />
+                  </div>
                 </div>
-              </div>
-              <div className="mb-2 flex justify-between">
-                <div>Status</div>
-                {product.stock > 0 ? (
-                  <Badge variant="outline">In Stock</Badge>
-                ) : (
-                  <Badge variant="destructive">Out of Stock</Badge>
+                <div className="mb-2 flex justify-between">
+                  <div>Status</div>
+                  {product.stock > 0 ? (
+                    <Badge variant="outline">In Stock</Badge>
+                  ) : (
+                    <Badge variant="destructive">Out of Stock</Badge>
+                  )}
+                </div>
+                {product.stock > 0 && (
+                  <div className="flex-center">
+                    <Button className="w-full">Add to Cart</Button>
+                  </div>
                 )}
-              </div>
-              {product.stock > 0 && (
-                <div className="flex-center">
-                  <Button className="w-full">Add to Cart</Button>
-                </div>
-              )}
-            </CardContent>
+              </CardContent>
+            </Card>
           </div>
         </div>
       </section>
--- types/index.ts
+++ types/index.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 import { insertProductSchema } from '@/lib/validators';
 
 export type Product = z.infer<typeof insertProductSchema> & {
-    id: number;
+    id: string;
     rating: string;
     createdAt: Date;
 }
