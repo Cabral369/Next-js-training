9fa2dc23bdb3e286cc0548944c8429c2e60e678e
Nicolas Cabral <nicolas.cabral.carvalho@gmail.com>
2025-02-17 15:43:54 -0300

    Add computed properties for cart and order pricing, and utility functions for ID formatting and date handling

--- lib/utils.ts
+++ lib/utils.ts
@@ -61,3 +61,44 @@ export function formatCurrency(amount: number | string | null){
     return 'NaN'
   }
 }
+
+export function formatId(id: string){
+  return `..${id.substring(id.length - 6)}`
+}
+
+//format date and times
+export function formatDate(dateString: Date){
+  const dateTimeOptions: Intl.DateTimeFormatOptions = {
+    month: 'short',
+    year: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true
+  };
+
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    weekday:'short',
+    month: 'short',
+    year: 'numeric',
+    day: 'numeric'
+  };
+
+  const timeOptions: Intl.DateTimeFormatOptions = {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true
+  }
+
+  const formattedDateTime: string = new Date(dateString).toLocaleDateString('pt-BR', dateTimeOptions);
+
+  const formattedDate: string = new Date(dateString).toLocaleDateString('pt-BR', dateOptions);
+
+  const formattedTime: string = new Date(dateString).toLocaleTimeString('pt-BR', timeOptions);
+
+  return{
+    dateTime: formattedDateTime,
+    dateOnly: formattedDate,
+    timeOnly: formattedTime
+  }
+}
