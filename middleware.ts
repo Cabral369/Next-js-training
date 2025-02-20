e6922fe9e83c3eaa7f88a3df2f5277e9343e7813
Nicolas Cabral <nicolas.cabral.carvalho@gmail.com>
2025-02-20 10:46:45 -0300

    Implement middleware for admin route protection and add unauthorized access page

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,23 @@
-export {  auth as middleware } from './auth';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { auth } from "./auth";
+
+export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith("/admin")) {
+    const session = await auth();
+
+    if (!session) {
+      return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
+
+    if (session?.user?.role !== "admin") {
+      return NextResponse.redirect(new URL("/unauthorized", request.url));
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/admin/:path*",
+};
