94afe492484959af250f144d111bf8fccc01f979
Nicolas Cabral <nicolas.cabral.carvalho@gmail.com>
2025-02-21 17:15:19 -0300

    Update pagination condition and increase latest products limit to 10

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -74,7 +74,7 @@ const ProductsPage = async (props: {
             ))}
           </TableBody>
         </Table>
-        {products?.totalPages && products.totalPages > 1 && (
+        {products.totalPages > 1 && (
           <Pagination page={page} totalPages={products.totalPages} />
         )}
       </div>
