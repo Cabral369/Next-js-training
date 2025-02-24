de99526b3f7a63537a69d3e3e4ee40724c2c8531
Nicolas Cabral <nicolas.cabral.carvalho@gmail.com>
2025-02-24 13:05:06 -0300

    Add admin user management page with user listing and pagination

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -13,6 +13,7 @@ import { prisma } from "@/db/prisma";
 import { formatErrorMessages } from "../utils";
 import { ShippingAddress } from "@/types";
 import { z } from "zod";
+import { PAGE_SIZE } from "../constants";
 
 export async function signInWwithCredentials(
   prevState: unknown,
@@ -167,4 +168,25 @@ export async function updateProfile(user: { name: string; email: string }) {
   } catch (error) {
     return { success: false, message: formatErrorMessages(error) };
   }
+}
+
+export async function getAllUsers({
+  limit = PAGE_SIZE,
+  page,
+}: {
+  limit?: number;
+  page: number;
+}) {
+  const data = await prisma.user.findMany({
+    orderBy: { createdAt: "desc" },
+    take: limit,
+    skip: (page - 1) * limit,
+  });
+
+  const dataCount = await prisma.user.count();
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
 }
