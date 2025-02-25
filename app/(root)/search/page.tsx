69372a0f6fa8d439b278dbcbd84d7bfb0ce5790e
Nicolas Cabral <nicolas.cabral.carvalho@gmail.com>
2025-02-25 16:26:33 -0300

    Add dynamic metadata generation for search page based on query parameters

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -5,6 +5,7 @@ import {
   getAllProducts,
 } from "@/lib/actions/products.actions";
 import Link from "next/link";
+import { title } from "process";
 
 const prices = [
   { name: "R$0 to R$50", value: "0-50" },
@@ -16,6 +17,42 @@ const prices = [
 ];
 
 const ratings = [4, 3, 2, 1];
+
+export async function generateMetadata(props: {
+  searchParams: Promise<{
+    q: string;
+    category: string;
+    price: string;
+    rating: string;
+  }>;
+}) {
+  const {
+    q = "all",
+    category = "all",
+    price = "all",
+    rating = "all",
+  } = await props.searchParams;
+
+  const isQuerySet = q && q !== "all" && q.trim() !== "";
+  const isCategorySet =
+    category && category !== "all" && category.trim() !== "";
+  const isPriceSet = price && price !== "all" && price.trim() !== "";
+  const isRatingSet = rating && rating !== "all" && rating.trim() !== "";
+
+  if (isQuerySet || isCategorySet || isPriceSet || isRatingSet) {
+    return {
+      title: `Search ${isQuerySet ? q : ""} ${
+        isCategorySet ? `: Category ${category}` : ""
+      } ${isPriceSet ? `: Price ${price}` : ""} ${
+        isRatingSet ? `: Rating ${rating}` : ""
+      }`,
+    };
+  } else {
+    return {
+      title: "Search Products",
+    };
+  }
+}
 
 const SearchPage = async (props: {
   searchParams: Promise<{
