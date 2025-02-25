70dc96fb10e5eec3c8bc5ba1d52f094946e9f1c1
Nicolas Cabral <nicolas.cabral.carvalho@gmail.com>
2025-02-25 09:35:30 -0300

    Add search functionality to admin orders page with filter display

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -19,25 +19,38 @@ export const metadata: Metadata = {
 };
 
 const AdminOrdersPage = async (props: {
-  searchParams: Promise<{ page: string }>;
+  searchParams: Promise<{ page: string; query: string }>;
 }) => {
-  const { page = "1" } = await props.searchParams;
+  const { page = "1", query: searchText } = await props.searchParams;
 
   const orders = await getAllOrders({
     page: Number(page),
-    limit: 8,
+    query: searchText,
   });
 
   return (
     <>
       <div className="space-y-2">
-        <h2 className="h2-bold">Orders</h2>
+        <div className="flex items-center gap-3">
+          <h1 className="h2-bold">Orders</h1>
+          {searchText && (
+            <div>
+              Filtered by <i>&quot;{searchText}&quot;</i>{" "}
+              <Link href="/admin/orders">
+                <Button variant="outline" size="sm">
+                  Remove Filter
+                </Button>
+              </Link>
+            </div>
+          )}
+        </div>
         <div className="overflow-x-auto">
           <Table>
             <TableHeader>
               <TableRow>
                 <TableHead>ID</TableHead>
                 <TableHead>DATE</TableHead>
+                <TableHead>NAME</TableHead>
                 <TableHead>TOTAL</TableHead>
                 <TableHead>PAID</TableHead>
                 <TableHead>DELIVERED</TableHead>
@@ -49,6 +62,7 @@ const AdminOrdersPage = async (props: {
                 <TableRow key={item.id}>
                   <TableCell>{formatId(item.id)}</TableCell>
                   <TableCell>{formatDate(item.createdAt).dateTime}</TableCell>
+                  <TableCell>{item.user.name}</TableCell>
                   <TableCell>{formatCurrency(item.totalPrice)}</TableCell>
                   <TableCell>
                     {item.isPaid && item.paidAt
