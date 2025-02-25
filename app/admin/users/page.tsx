7675dfa0b65be48c1b6ae43cf2dcb10a101491cc
Nicolas Cabral <nicolas.cabral.carvalho@gmail.com>
2025-02-25 09:44:21 -0300

    Add user filtering functionality to admin users page

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -22,16 +22,29 @@ export const metadata: Metadata = {
 const AdminUserPage = async (props: {
   searchParams: Promise<{
     page: number;
+    query: string;
   }>;
 }) => {
-  const { page = "1" } = await props.searchParams;
+  const { page = "1", query: searchText } = await props.searchParams;
 
-  const users = await getAllUsers({ page: page as number });
+  const users = await getAllUsers({ page: page as number, query: searchText });
 
   return (
     <>
       <div className="space-y-2">
-        <h2 className="h2-bold">Orders</h2>
+        <div className="flex items-center gap-3">
+          <h1 className="h2-bold">Users</h1>
+          {searchText && (
+            <div>
+              Filtered by <i>&quot;{searchText}&quot;</i>{" "}
+              <Link href="/admin/users">
+                <Button variant="outline" size="sm">
+                  Remove Filter
+                </Button>
+              </Link>
+            </div>
+          )}
+        </div>
         <div className="overflow-x-auto">
           <Table>
             <TableHeader>
