11985863a0b84b32c419afd53e09f2091140aa1d
Nicolas Cabral <nicolas.cabral.carvalho@gmail.com>
2025-02-25 10:16:29 -0300

    Add category drawer component to header for category selection

--- lib/actions/products.actions.ts
+++ lib/actions/products.actions.ts
@@ -141,3 +141,13 @@ export async function updateProduct(data: z.infer<typeof updateProductSchema>) {
     return { success: false, message: formatErrorMessages(error) };
   }
 }
+
+//get all categories
+export async function getAllCategories() {
+  const data = await prisma.product.groupBy({
+    by: ["category"],
+    _count: true,
+  });
+
+  return data;
+}
